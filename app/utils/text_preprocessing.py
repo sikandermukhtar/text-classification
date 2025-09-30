e5a17f44fa0dec123bd13e07cb2463e7874ecfa7
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2025-09-30 10:14:57 +0500

    adds html tag removing in text preprocessing pipeline

--- app/utils/text_preprocessing.py
+++ app/utils/text_preprocessing.py
@@ -4,8 +4,13 @@ def remove_url(text):
     pattern = re.compile(r'https?://\S+|www\.\S+')
     return pattern.sub(r'', text)
 
+def remove_html_tags(text):
+  pattern = re.compile(r'<.*?>')
+  return pattern.sub(r'', text)
+
 def clean_text(text):
 
+    text = remove_html_tags(text)
     text = remove_url(text)
 
     return text
